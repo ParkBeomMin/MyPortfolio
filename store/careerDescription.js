0acaf414e2ccadf1d34ff039af181a17471aa118
bmpark <bmpark@jinhak.com>
2023-07-27 16:28:20 +0900

    경력기술서 데이터..

---
+++ store/careerDescription.js
@@ -0,0 +1,50 @@
+export const state = () => ({
+  name: "박범민",
+  email: "club20608@gmail.com",
+  intro: "안녕하세요. 박범민입니다.",
+  skills: [
+    {
+      name: "Javascript",
+      description: ["ES6+ 문법 활용 가능"],
+    },
+    {
+      name: "Typescript",
+      description: "",
+    },
+    {
+      name: "MSSQL",
+      description: [""],
+    },
+    {
+      name: "PostgreSQL",
+      description: "",
+    },
+    {
+      name: "Vue",
+      description: ["SPA 구조 이해", "Component"],
+    },
+    {
+      name: "Nuxt",
+      description: "",
+    },
+    {
+      name: "Node",
+      description: "",
+    },
+  ],
+  careers: [
+    {
+      company: "진학사",
+      department: "캐치본부 개발팀",
+      workDate: "2020.9.29 ~ 재직중",
+      description: "캐치 웹 서비스 유지보수 및 신규 기능 개발",
+    },
+    {
+      company: "진학사",
+      department: "신사업본부",
+      workDate: "2019.12.02 ~ 2020.9.28",
+      description: "신규 사업 탐색 및 아이디에이션",
+    },
+  ],
+  description: [],
+});
